cec3df2a781b39ae79dcfa98558168263b539df5
avantika0111 <46592544+avantika0111@users.noreply.github.com>
2019-12-21 16:02:08 +0530

    Create TCPDateServer.java

---
+++ TCPDateServer.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class TCPDateServer
+{
+    public static void main(String args[]) throws Exception
+    {
+        ServerSocket s=new ServerSocket(5217);
+            System.out.println("Server ready to connect ...");
+            Socket soc=s.accept();
+            DataOutputStream out=new DataOutputStream(soc.getOutputStream());
+            out.writeBytes("Server Date : " + (new Date()).toString() + "\n");
+			System.out.println("Done!!\nServer Exiting!!!");
+            out.close();
+            soc.close();     
+    }
+}
